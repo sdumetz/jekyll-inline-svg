4332c54a13dd848522d1a8215458d861c53f39f1
Sebastien Dumetz <s.dumetz@holusion.com>
2017-08-22 11:34:46 +0200

    add some more comment to documentation

--- lib/jekyll-inline-svg.rb
+++ lib/jekyll-inline-svg.rb
@@ -2,6 +2,7 @@ require 'svg_optimizer'
 require 'shellwords'
 require 'jekyll/liquid_extensions'
 class RemoveSize < SvgOptimizer::Plugins::Base
+  # remove "width" and "height" attributes
   def process
     xml.root.remove_attribute("height")
     xml.root.remove_attribute("width")
@@ -17,15 +18,18 @@ PLUGINS = SvgOptimizer::DEFAULT_PLUGINS.delete_if {|plugin|
   RemoveSize
 ]
 
-#logger.info(PLUGINS)
+
 module Jekyll
   module Tags
     class JekyllInlineSvg < Liquid::Tag
-
-      include Jekyll::LiquidExtensions #import lookup_variable function
+      #import lookup_variable function
       # https://github.com/jekyll/jekyll/blob/master/lib/jekyll/liquid_extensions.rb
+      include Jekyll::LiquidExtensions
+
       # For interpoaltion, look for liquid variables
       VARIABLE = /\{\{\s*([\w]+\.?[\w]*)\s*\}\}/i
+
+      #Separate file path from other attributes
       PATH_SYNTAX = %r!
         ^(?<path>[^\s"']+|"[^"]*"|'[^']*')
         (?<params>.*)
@@ -34,17 +38,19 @@ module Jekyll
       def initialize(tag_name, input, tokens)
         super
 
-        #@logger = Logger.new(STDOUT)
-        #@logger.level = Logger::INFO
         @svg, @params = JekyllInlineSvg.parse_params(input)
-        #@logger.info(@svg +", "+@width)
       end
+
+      #lookup Liquid variables from markup in context
       def interpolate(markup, context)
         markup.scan VARIABLE do |variable|
           markup = markup.gsub(VARIABLE, lookup_variable(context, variable.first))
         end
         markup
       end
+
+      #Parse parameters. Returns : [svg_path, parameters]
+      # Does not interpret variables as it's done at render time
       def self.parse_params(input)
         matched = input.strip.match(PATH_SYNTAX)
         return matched["path"].strip.gsub("\"","").gsub("'",""), matched["params"].strip
